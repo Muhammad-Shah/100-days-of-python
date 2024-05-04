5889595c3e441385d0906e8135e7b261e4f40e83
Muhammad-Shah <msh87376@gmail.com>
2024-05-04 22:46:13 +0500

    complete

---
+++ DAY_04/day4.1.py
@@ -0,0 +1,15 @@
+# Randomization in python
+import random
+
+randm_int = random.randint(1,100) # return an integer between 1 and 100 including 1 and 100
+print(randm_int)
+
+# return random float numbers ranges between 0.0 to 1.0 not including 0 1nd 1 ()
+randFloat = random.random()
+print(randFloat)
+
+# 
+randFloatDec = random.random() * 5
+print(round(randFloatDec,2))
+
+
