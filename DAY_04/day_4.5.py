f4270b4b25a333fec35261a94c3a7008c18ec320
Muhammad-Shah <msh87376@gmail.com>
2024-05-05 22:34:26 +0500

    complete

---
+++ DAY_04/day_4.5.py
@@ -0,0 +1,13 @@
+# Nested list
+dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
+
+fruits = ["Strawberries", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen_nested = [fruits, vegetables]
+
+print(dirty_dozen_nested)
+
+
+
