58e9223ee88a526fa408aea3d91c97b9c582003d
Muhammad-Shah <msh87376@gmail.com>
2024-05-16 23:42:30 +0500

    day-5

---
+++ DAY_05/054.py
@@ -0,0 +1,28 @@
+# Program that automatically print FizzBuzz
+
+
+# Method : 1
+# for number in range(1, 101):
+#     if number % 3 != 0 and number % 5 != 0:
+#         print(number)
+#     else:
+#         if number % 3 == 0 and number % 5 == 0:
+#             print("FizzBuzz")
+#         elif number % 3 == 0:
+#             print("Fizz")
+#         elif number % 5 == 0:
+#             print("Buzz")
+
+
+
+# Method : 2
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
+        
