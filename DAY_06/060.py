fa654ebf4181c76a66ea1510323e7323077ca1a7
Muhammad-Shah <msh87376@gmail.com>
2024-05-17 22:32:13 +0500

    day-5

---
+++ DAY_06/060.py
@@ -0,0 +1,11 @@
+# Indentation in Python 
+
+sky = input()
+
+def my_function():
+    if sky == "clear":  # 4 spaces indentation
+        print("Sky is Blue")    # 8 spaces indentation
+    elif sky == "cloudy":
+        print("Sky is Grey")
+
+my_function()
