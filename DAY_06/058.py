6b660716de30e8ad3f60d1a62f94f64be7e6d509
Muhammad-Shah <msh87376@gmail.com>
2024-05-17 22:31:33 +0500

    day-5

---
+++ DAY_06/058.py
@@ -0,0 +1,14 @@
+# Built in Functions
+# print("Hello This is Python Programming")
+# number_of_char = len("Hello This is Python Programming")
+# print(number_of_char)
+
+
+
+# In order to define our own function
+def my_function():
+    print("Hello Manchester")
+    print("By Manchester")
+
+# Call to my function
+my_function()
