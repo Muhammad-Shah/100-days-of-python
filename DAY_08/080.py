ca97a7b3783282a6181a8c2724e037abfb7a5a0e
Muhammad-Shah <msh87376@gmail.com>
2024-05-24 23:28:25 +0500

    day 8

---
+++ DAY_08/080.py
@@ -0,0 +1,16 @@
+
+def greet():
+    print("Hello!")
+    print("Good morning")
+    print("Whats Up?\n\n")
+
+greet()
+
+# Function that takes input
+
+def greet_with_input(name):
+    print(f"Hello! {name}")
+    print(f"Good morning {name}")
+    print(f"Whats Up {name}?\n\n")
+
+greet_with_input("Muhammad Shah")
