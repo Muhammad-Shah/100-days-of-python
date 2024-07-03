da7979d2a1540bb239170afe2b31d83c76c7a50c
Muhammad-Shah <msh87376@gmail.com>
2024-07-03 23:05:08 +0500

    day 12

---
+++ DAY_12/118.py
@@ -0,0 +1,38 @@
+# from random import randint
+# print("Welcome to the Guessing Game!")
+# print("I'm thinking of a number between 1 and 100.")
+# dificulty_level = input("Choose a dificulty. Type 'easy' or 'hard': ")
+# GUESSED = randint(1, 100)
+
+# def guess_fun(attempts):
+#     print(f"You have {attempts} remaining attempts left")
+#     while attempts != 0:
+#         user_guess = int(input("Make a Guess: "))
+#         attempts -= 1
+#         if user_guess > GUESSED:
+#             print("Too high.\nGuess again")
+#             print(f"You have {attempts} remaining attempts left")
+#         elif user_guess < GUESSED:
+#             print("Too low.\n Guess again")
+#             print(f"You have {attempts} remaining attempts left")
+#         else:
+#             print(f"You got it the answer is {GUESSED}")
+#             attempts = 0
+
+
+# if dificulty_level == "hard":
+#     attempts = 5
+#     guess_fun(attempts)
+# else:
+#     attempts = 10
+#     guess_fun(attempts)
+
+# ************************************************************************
+
+
+
+
+    
+
+
+
